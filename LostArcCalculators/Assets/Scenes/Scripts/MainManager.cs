511202a3e4c8a17eb747ad70b6c715a92dc7cc10
yusuf mete saracoglu <yusufmetesaracoglu@gmail.com>
2022-03-20 17:55:37 +0300

    +Add: Calculating for expected rate was added.

--- LostArcCalculators/Assets/Scenes/Scripts/MainManager.cs
+++ LostArcCalculators/Assets/Scenes/Scripts/MainManager.cs
@@ -13,12 +13,104 @@ public class MainManager : Singleton<MainManager>
 
     [SerializeField]  private List<int> _marktePriceList = new List<int>();
 
+    [SerializeField] private List<int> _itemsVendorCountList = new List<int>();
+    [SerializeField] private List<int> _itemsVendorPriceList = new List<int>();
+    [SerializeField] private List<int> _itemsProfitList = new List<int>();
+
+    [SerializeField] private int _totalVenderPrice = 0;
+    [SerializeField] private int _totalProfit = 0;
+    [SerializeField] private float _lastRate = 0;
+
+
     void Start()
     {
         LoadMarketPrice();
         ResetPanel();
         CreateListItems();
     }
+
+    private void CalculateBest()
+    {
+        if(float.TryParse(UIPanelItemList.Input_1.text.ToString(), out float expectedRate))
+        {
+            int itemCount = (int)ItemCodes.MAX;
+            _itemsVendorCountList = new List<int>(new int[itemCount]);
+            _itemsVendorPriceList = new List<int>(new int[itemCount]);
+            _itemsProfitList = new List<int>(new int[itemCount]);
+            _lastRate = 0;
+            _totalVenderPrice = 0;
+            _totalProfit = 0;
+
+            while (true)
+            {
+                int bestItemIndex = -1;
+                float bestRate = 0;
+                int bestMarketPrice = 0;
+                int bestVendorPrice= 0;
+
+                for (int i = 0; i < itemCount; i++)
+                {
+                    ItemCodes currentItem = (ItemCodes)i;
+                    Debug.Log($"{i}");
+                    int currentVendorPrice = ResourceManager.GetCurrentRankVendorPrice(currentItem, _itemsVendorCountList[i]);
+                    int currentMarketPrice = GetMarketValue(currentItem);
+                    float currentRate = (float)currentMarketPrice / currentVendorPrice;
+                    if (currentRate > bestRate)
+                    {
+                        bestRate = currentRate;
+                        bestItemIndex = i;
+                        bestMarketPrice = currentMarketPrice;
+                        bestVendorPrice = currentVendorPrice;
+                    }
+                }
+
+                if (bestItemIndex == -1 || ((float)(_totalProfit + bestMarketPrice) / (_totalVenderPrice + bestVendorPrice)) < expectedRate)
+                    break;
+
+                _totalVenderPrice += bestVendorPrice;
+                _totalProfit += bestMarketPrice;
+                _lastRate = (float)_totalProfit / _totalVenderPrice;
+
+                _itemsVendorCountList[bestItemIndex] += 1;
+                _itemsVendorPriceList[bestItemIndex] += bestVendorPrice;
+                _itemsProfitList[bestItemIndex] += bestMarketPrice;
+            }
+        }
+        else
+        {
+            Debug.Log("Float not valid: " + UIPanelItemList.Input_1.ToString());
+        }
+    }
+
+
+
+    private void SetTables()
+    {
+        var allItems = UIPanelItemList.AllItems;
+
+        for (int i = 0; i < allItems.Count; i++)
+        {
+            if (i < _itemsVendorCountList.Count)
+                allItems[i].Output_1.text = _itemsVendorCountList[i].ToString() + "x";
+            else
+                allItems[i].Output_1.text = "0x";
+
+            if (i < _itemsVendorPriceList.Count)
+                allItems[i].Output_2.text = _itemsVendorPriceList[i].ToString() + ResourceManager.CS_ICON;
+            else
+                allItems[i].Output_2.text = $"0{ResourceManager.CS_ICON}";
+
+            if (i < _itemsProfitList.Count)
+                allItems[i].Output_3.text = _itemsProfitList[i].ToString() + ResourceManager.GOLD_ICON;
+            else
+                allItems[i].Output_3.text = $"0{ResourceManager.GOLD_ICON}";
+        }
+
+        UIPanelItemList.ResultOutput_1.text = _totalVenderPrice.ToString() + ResourceManager.CS_ICON;
+        UIPanelItemList.ResultOutput_2.text = _totalProfit.ToString() + ResourceManager.GOLD_ICON;
+        UIPanelItemList.ResultOutput_3.text = $"1{ResourceManager.CS_ICON} = {_lastRate}{ResourceManager.GOLD_ICON}";
+    }
+
 
 
     private void ResetPanel()
@@ -88,8 +180,16 @@ public class MainManager : Singleton<MainManager>
     private int GetMarketValue(ItemCodes code)
     {
         int c = (int)code;
-        if (c < _marktePriceList.Count)
+
+        if (_marktePriceList!=null && c < _marktePriceList.Count)
+        {
+            if(code == ItemCodes.SolarGrace)
+                return _marktePriceList[c] * 7;
+            else if(code == ItemCodes.SolarBlessing)
+                return _marktePriceList[c] * 2;
+
             return _marktePriceList[c];
+        }
         else
             return 0;
     }
@@ -118,6 +218,8 @@ public class MainManager : Singleton<MainManager>
     {
         ReadMarketPrice();
         SaveMarketPrice();
+        CalculateBest();
+        SetTables();
     }
 
 }
--- LostArcCalculators/Assets/Scenes/Scripts/ResourceManager.cs
+++ LostArcCalculators/Assets/Scenes/Scripts/ResourceManager.cs
@@ -31,5 +31,23 @@ public class ResourceManager : Singleton<ResourceManager>
     }
 
 
+    private static readonly int[][] VendorPrices = new int[][]{
+        new int[] { 100,130,170,200,230,270,300,340,380,420,460,510,560,610,660,720,int.MaxValue }, //Honor Leapstone 
+        new int[] { 130,170,200,240,280,310,360,400,440,490,540,600,650,710,int.MaxValue }, //Great Honor Leapstone
+        new int[] { 500,570,650,730,820,910,1010,int.MaxValue }, //Honor Shard Pouch (L)
+        new int[] { 550,630,710,800,890,990,1100, int.MaxValue }, //Solar Grace
+        new int[] { 570,650,730,820,920,1020,1130, int.MaxValue }, //Solar Blessing
+        new int[] { 590,670,760,850,950,1060,1170, int.MaxValue }, //Solar Protection
+        new int[] { 60,90,120,140,170,200,230,260,290,330,360,400,430,470,510,560,600,650,int.MaxValue}, //Destruction Stone Crystal
+        new int[] { 10,40,60,80,100,120,140,160,190,210,230,250,280,300,320,350,370,400,420,450,470,500,530,int.MaxValue }, //Guartian Stone Crystal
+        new int[] { 1470,1640,1820,int.MaxValue }, //Powder of Sage
+    };
+
+    public static int GetCurrentRankVendorPrice(ItemCodes itemCode, int rank)
+    {
+        int length = VendorPrices[(int)itemCode].Length;
+        return VendorPrices[(int)itemCode][Mathf.Clamp(rank, 0, length-1)];
+    }
+
 
 }
